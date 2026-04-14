580f635869d748c2106e5dba3bab43e8caa3bf18
debidong <1953531014@qq.com>
2026-04-14 18:09:28 +0800

    feat: fall back to runtime/debug.ReadBuildInfo for version info
    When ldflags are not set (e.g., via go install), read VCS metadata
    embedded by the Go toolchain so users get meaningful version output
    instead of "dev (none) built unknown".

--- cmd/flashduty/main.go
+++ cmd/flashduty/main.go
@@ -3,20 +3,57 @@ package main
 import (
 	"fmt"
 	"os"
+	"runtime/debug"
 
 	"github.com/flashcatcloud/flashduty-cli/internal/cli"
 )
 
 var (
-	version = "dev"
-	commit  = "none"
-	date    = "unknown"
+	version = ""
+	commit  = ""
+	date    = ""
 )
 
 func main() {
+	if version == "" {
+		readBuildInfo()
+	}
+	if version == "" {
+		version = "dev"
+	}
+	if commit == "" {
+		commit = "none"
+	}
+	if date == "" {
+		date = "unknown"
+	}
 	cli.SetVersionInfo(version, commit, date)
 	if err := cli.Execute(); err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
 		os.Exit(1)
+	}
+}
+
+func readBuildInfo() {
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return
+	}
+	version = info.Main.Version
+	for _, s := range info.Settings {
+		switch s.Key {
+		case "vcs.revision":
+			if len(s.Value) > 7 {
+				commit = s.Value[:7]
+			} else {
+				commit = s.Value
+			}
+		case "vcs.time":
+			date = s.Value
+		case "vcs.modified":
+			if s.Value == "true" {
+				version += "-dirty"
+			}
+		}
 	}
 }
